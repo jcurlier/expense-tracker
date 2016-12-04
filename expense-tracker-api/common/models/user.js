2c9937b8b9699fad497afb21f63c9e3bbf9ba9b6
Jerome Curlier <jcurlier@gmail.com>
2016-12-03 16:54:38 -0800

    add get role method for user model

--- expense-tracker-api/common/models/user.js
+++ expense-tracker-api/common/models/user.js
@@ -2,6 +2,36 @@
 
 module.exports = function(User) {
 
+    User.getRoles = function (id, cb) {
+      User.getApp(function (err, app) {
+        if (err) throw err;
+        var RoleMapping = app.models.RoleMapping;
+        var Role = app.models.Role;
+        RoleMapping.find({ where : { principalId: id }}, function (err, roleMappings) {
+            var roleIds = roleMappings.map(function (roleMapping) {
+            return roleMapping.roleId;
+            });
+            var conditions = roleIds.map(function (roleId) {
+            return { id: roleId };
+            });
+            Role.find({ where: { or: conditions}}, function (err, roles) {
+            if (err) throw err;
+            var roleNames = roles.map(function(role) {
+                return role.name;
+            });
+            cb(null, roleNames);
+        });
+      });
+    });
+  };
+  User.remoteMethod('getRoles', {
+    description: 'Queries roles of user.',
+    http: { path: '/:id/roles', verb: 'get' },
+    accepts: {arg: 'id', type: 'string', required: 'true', description: 'User Id'},
+    returns: {type: 'array', root: true},
+    isStatic: true
+  });
+  
   User.disableRemoteMethod("create", true);
   User.disableRemoteMethod("upsert", true);
   User.disableRemoteMethod("upsertWithWhere", true);
