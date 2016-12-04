c177467adb518567b504c52531c6541bb021ed72
Jerome Curlier <jcurlier@gmail.com>
2016-12-04 14:53:18 -0800

    remove unused chart refresh attempts for chart update bug

--- expense-tracker-web/README.md
+++ expense-tracker-web/README.md
@@ -30,3 +30,7 @@ Run `ng github-pages:deploy` to deploy to Github Pages.
 ## Further help
 
 To get more help on the `angular-cli` use `ng --help` or go check out the [Angular-CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Bugs
+
+* [AngularJS 2 Chart Bug on Filter](https://github.com/valor-software/ng2-charts/issues/477
--- expense-tracker-web/src/app/report/report.component.ts
+++ expense-tracker-web/src/app/report/report.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, SimpleChange } from '@angular/core';
 import { LoopBackConfig } from './../shared/sdk/index';
 import { BASE_URL, API_VERSION } from './../shared/base.url';
 import { UserApi } from '../shared/sdk/services';
+import {BaseChartDirective} from 'ng2-charts/ng2-charts';
 
 @Component({
   selector: 'app-report',
@@ -9,6 +10,10 @@ import { UserApi } from '../shared/sdk/services';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
+  private data;
+  private startDate: Date;
+  private endDate: Date;
+  @ViewChild( BaseChartDirective ) chart: BaseChartDirective;
 
   private datasets = [
     {
@@ -59,7 +64,7 @@ export class ReportComponent implements OnInit {
     this.userApi.weeklyReport(this.userApi.getCurrentId()).subscribe((data) => {
 
       if (data && data.length > 0) {
-
+        this.data = data;
         var labels = new Array(data.length);
         var results = new Array(data.length);
 
@@ -72,5 +77,24 @@ export class ReportComponent implements OnInit {
         this.datasets[0].data = results;
       }
     });
+  }
+
+    filter(): void {
+      if (this.data && this.data.length > 0) {
+
+        var labels = new Array(this.data.length);
+        var results = new Array(this.data.length);
+
+        for (var i=0; i<this.data.length; i++) {
+          if (new Date(Date.parse(this.data[i].weekStartDate)) >= this.startDate 
+                && new Date(Date.parse(this.data[i].weekEndDate)) <= this.endDate) {
+            labels[i] = this.data[i].yearWeek.toString().substr(0, 4) + '/' + this.data[i].yearWeek.toString().substr(4);
+            results[i] = this.data[i].total;
+          }
+        }
+
+        this.labels = labels;
+        this.datasets[0].data = results;
+      }
   }
 }
