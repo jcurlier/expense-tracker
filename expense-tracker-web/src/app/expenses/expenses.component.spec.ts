cc90aebc69dfc7e381842e364860b39b1d29131c
Jerome Curlier <jcurlier@gmail.com>
2016-12-03 20:52:54 -0800

    add expense component created with ng generate module Login

---
+++ expense-tracker-web/src/app/expenses/expenses.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ExpensesComponent } from './expenses.component';
+
+describe('ExpensesComponent', () => {
+  let component: ExpensesComponent;
+  let fixture: ComponentFixture<ExpensesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExpensesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpensesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ expense-tracker-web/src/app/expenses/expenses.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExpensesComponent } from './expenses.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ExpensesComponent]
+})
+export class ExpensesModule { }
