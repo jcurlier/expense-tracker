79277237ef8a655a9e4febdd39a59cd969827392
Jerome Curlier <jcurlier@gmail.com>
2016-12-03 16:49:02 -0800

    hide api methods not used and generated by default

--- expense-tracker-api/common/models/expense.js
+++ expense-tracker-api/common/models/expense.js
@@ -1,10 +1,20 @@
-'use strict';
-
 module.exports = function(Expense) {
 
   Expense.beforeRemote('create', function(context, user, next) {
     context.args.data.ownerId = context.req.accessToken.userId;
     next();
   });
-  
+
+  Expense.disableRemoteMethod("upsert", true);
+  Expense.disableRemoteMethod("upsertWithWhere", true);
+  Expense.disableRemoteMethod("updateAll", true);
+  Expense.disableRemoteMethod("replaceOrCreate", true);
+  Expense.disableRemoteMethod("replaceById", true);
+  Expense.disableRemoteMethod("findById", true);
+  Expense.disableRemoteMethod("findOne", true);
+  Expense.disableRemoteMethod("count", true);
+  Expense.disableRemoteMethod("exists", true);
+  Expense.disableRemoteMethod('createChangeStream', true);	
+  Expense.disableRemoteMethod('__get__owner', false);
+
 };
--- expense-tracker-api/common/models/user.js
+++ expense-tracker-api/common/models/user.js
@@ -2,4 +2,35 @@
 
 module.exports = function(User) {
 
+  User.disableRemoteMethod("create", true);
+  User.disableRemoteMethod("upsert", true);
+  User.disableRemoteMethod("upsertWithWhere", true);
+  User.disableRemoteMethod("updateAll", true);
+  User.disableRemoteMethod("updateAttributes", false);
+  User.disableRemoteMethod("replaceOrCreate", true);
+  User.disableRemoteMethod("replaceById", true);
+  User.disableRemoteMethod("find", true);
+  User.disableRemoteMethod("findById", true);
+  User.disableRemoteMethod("findOne", true);
+  User.disableRemoteMethod("deleteById", true);
+  User.disableRemoteMethod("confirm", true);
+  User.disableRemoteMethod("count", true);
+  User.disableRemoteMethod("exists", true);
+  User.disableRemoteMethod("resetPassword", true);
+  User.disableRemoteMethod('createChangeStream', true);	
+
+  User.disableRemoteMethod('__count__accessTokens', false);
+  User.disableRemoteMethod('__create__accessTokens', false);
+  User.disableRemoteMethod('__delete__accessTokens', false);
+  User.disableRemoteMethod('__destroyById__accessTokens', false);
+  User.disableRemoteMethod('__findById__accessTokens', false);
+  User.disableRemoteMethod('__get__accessTokens', false);
+  User.disableRemoteMethod('__updateById__accessTokens', false);
+
+  User.disableRemoteMethod('__create__expenses', false);
+  User.disableRemoteMethod('__delete__expenses', false);
+  User.disableRemoteMethod('__findById__expenses', false);
+  User.disableRemoteMethod('__updateById__expenses', false);
+  User.disableRemoteMethod('__destroyById__expenses', false);
+  User.disableRemoteMethod('__count__expenses', false);
 };
