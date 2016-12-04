54f17f75b7dc74a646d33ad16c56e20e7043d47f
Jerome Curlier <jcurlier@gmail.com>
2016-12-03 21:03:45 -0800

    update admin compponent

--- expense-tracker-web/src/app/admin/admin.component.ts
+++ expense-tracker-web/src/app/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LoopBackConfig } from './../shared/sdk/index';
+import { BASE_URL, API_VERSION } from './../shared/base.url';
+import { Expense } from '../shared/sdk/models';
+import { ExpenseApi } from '../shared/sdk/services';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  private expenses = [];
+
+  constructor(private expenseApi: ExpenseApi) { 
+    LoopBackConfig.setBaseURL(BASE_URL);
+    LoopBackConfig.setApiVersion(API_VERSION);
+    this.getExpenses();
+  }
 
   ngOnInit() {
   }
 
+  getExpenses(): void {
+      this.expenseApi.find({
+        include: [
+          {
+            relation: 'owner'
+          }
+        ],
+        order: 'date DESC',
+      }).subscribe((data) => {
+        this. expenses = data;
+      });
+  }
 }
